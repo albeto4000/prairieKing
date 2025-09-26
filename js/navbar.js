d9348af121584ce067effb47b2e6006bad927287
mdolin <mdolin608@gmail.com>
2025-09-26 17:24:49 -0400

    Added separate file for navbar

---
+++ js/navbar.js
@@ -0,0 +1,11 @@
+navbar = `<nav class="navbar navbar-dark bg-dark pt-3 pb-2 px-3 justify-content">
+    <a class="navbar-brand text-success" href="https://albeto4000.github.io/">MATTHEW DOLIN</a>
+    <div>
+        <div class="navbar-text text-capitalize">
+            ` + document.title + `
+        </div>
+    </div>
+    <button class="btn btn-outline-success btn-sm" data-toggle="modal" data-target="#aboutModal">About</button>
+</nav>`;
+
+document.write(navbar);
